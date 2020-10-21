1826a57e424f30da74e567e5af0bca44a399d4f0
GaloRB <barcenasgalo@gmail.com>
2020-10-21 14:05:39 -0500

    inciando protafolio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const typed = new Typed('.typed', {
+    strings: ['<i>Galo Rodrigo</i>', '<i>Soy  Desarrollador<br> Web</i>'],
+    typeSpeed: 50,
+    startDelay: 300,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+});
