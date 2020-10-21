889db19993383f9325f3d0fd8e4b0f84e24926a5
GaloRB <barcenasgalo@gmail.com>
2020-10-21 17:32:50 -0500

    prbando letra neon en banner nuevo color

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ particlesJS({
             }
         },
         "color": {
-            "value": "#ffffff"
+            "value": "#00aa9c"
         },
         "shape": {
             "type": "circle",
@@ -48,7 +48,7 @@ particlesJS({
         "line_linked": {
             "enable": true,
             "distance": 150,
-            "color": "#ffffff",
+            "color": "#0076e4",
             "opacity": 0.4,
             "width": 1
         },
